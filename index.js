59663b9254daacff021ee87924f113e6d1190d8e
PaoEsme <paesvero1983@gmail.com>
2023-10-01 18:43:37 -0600

    added pokemon POST

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+const pokemon = require('./routes/pokemon');
+
+/*"morgan" (en este caso nos ayuda a acotar de dónde viene el error) )es una dependencia de desarrollo, 
+no se utiliza cuando el servidor esta en desarrollo porque debería dar más información de la necesaria.*/
+app.use(morgan('dev'));
+/*El "app.use" sirve para añadir " "mideluers?"capas a nuestra aplicación, capas que procesaran 
+las peticiones y que les harán alguna modificación o revisaran algún dato*/
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /*
 Verbos HTTP (denotan una accion en particular)
 GET - obtener recurso (ejecuta en los navegadores cada vez que ponemos una direccion URL, regresan una pagina Web)
-POST - guardar y/o publicar algo 
-PATCH - actualizacion de datos de un recurso especifico 
-PUT - actualizacion de todos los elementos de datos  
+POST - guardar y/o publicar algo - almacenar/crear recursos
+PATCH - actualizacion de datos de un recurso especifico - modificar una parte de un recurso
+PUT - actualizacion de todos los elementos de datos - modificar un recurso
 DELETE - eliminar un recurso/registro de una base de datos 
 */
 
 app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido");
+    return res.status(200).json({code: 1, message: "Bienvenido al Pokedex"});
 });
 
-app.listen(3000, () => {
+app.use("/pokemon", pokemon);
+
+app.use((req, res, next) => {
+    return res.status(404).json({code: 404, message: "URL no encontrada"});
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    //"||" representa un <<or>>, si existe un valor poner un de los valores u otro que se mencione.
     console.log("Server is running...");
 });
---
+++ routes/pokemon.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const pokemon = express.Router(); 
+const db = require('../config/database')
+
+pokemon.post("/", async (req, res, next) => {
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+    if(pok_name && pok_height && pok_weight && pok_base_experience) {
+        let query = "INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
+        query += ` VALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+    
+        const rows = await db.query(query);
+        if(rows.affectedRows == 1) {
+            return res.status(201).json({ code: 201, message: "Pokemon insertado correctamente"});
+        }
+        
+        return res.status(500).json({ code: 500, message: "Ocurrio un error"});
+    }
+    return res.status(500).json({ code: 500, message: "Campos incompletos"});
+});
+
+pokemon.get("/", async (req, res, next) => {
+    const pkmn = await db.query("SELECT * FROM pokemon");
+    return res.status(200).json({ code: 1, message: pkmn });
+});
+
+/* como manejar datos especificos dentro de rutas */
+pokemon.get('/:id([0-9]{1,3})' , async (req, res, next) => {
+    const id= req.params.id - 1;
+    if (id >= 0 && id <= 150) {
+        const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id=" + id +":");
+        return res.status(200).json({code: 1, message: pkmn});
+    } 
+        /* como manejar datos no encontrados */
+    return res.status(404).send({code: 404, message: "Pokemon no encontrado"});
+});
+
+pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
+    const name = req.params.name;
+    //como manejar dentro de un arreglo 
+    /*for(i = 0; i < pokemon.length; i++) {
+        if (pokemon[i].name.toUpperCase() == name.toUpperCase()) {
+            return res.status(200).send(pokemon[i]);
+        }
+    }*/
+
+    //método filter para arreglos
+    //1const pkmn = pk.filter((p) => {
+    //El operador ternario se utiliza cuando queremos que de un "if" se retorne a algo    
+    //Este tiene la siguiente estructura:
+    /*condición + ? + valor si verdadero + : + valor si falso
+    return (p.name.toUpperCase() == name.toUpperCase()) ? p: null;*/
+        //Forma similar al operador ternario
+        //1return (p.name.toUpperCase() == name.toUpperCase()) && p;
+        //"&&" los apersans representan un <<and>>, las condiciónes deben ser verdaderas para obtener un resultado verdadero.
+    //1});
+
+    /*Es importante en cualquier lenguaje y mandemos inprimir en consola para saber si los valores estan bien o estan mal
+    console.log(pk);*/
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name='" + name +"':");
+    if (pkmn.length > 0) { 
+        return res.status(200).json({code: 1, message: pkmn});
+    }
+        return res.status(404).send({code: 1, message: "Pokemon no encontrado"});
+});
+
+//Manera más común de exportar cosas, pero solo permite hacerlo con una cosa.
+module.exports = pokemon;
